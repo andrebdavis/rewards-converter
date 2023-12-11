0556ff3134805013ba45aec6ebfca7bdf71e8503
andrebdavis <andreb.davis2@gmail.com>
2023-12-11 14:29:59 -0500

    added changes and classes

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,26 @@
 public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = cashToMiles(cashValue);
+    }
+
+    public RewardValue(double milesValue, boolean isMiles){
+        this.milesValue = milesValue;
+        this.cashValue = isMiles ? milesToCash(milesValue) : 0.0;
+    }
+
+    private double milesToCash(double milesValue) {
+        return milesValue * 0.0035;
+    }
+
+    private double cashToMiles(double cashValue) {
+        return cashValue / 0.0035;
+    }
+
+    public double getMilesValue() {
+        return milesValue;
+    }
 }
